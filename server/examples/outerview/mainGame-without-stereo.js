be4415c61083a7a75593817ef1d11b4e04ce92e1
Michiel van der Ros <michiel@xny.nl>
2015-11-01 11:35:41 +0100

    Transparent renderer with background css image showing

--- server/examples/outerview/mainGame-without-stereo.js
+++ server/examples/outerview/mainGame-without-stereo.js
@@ -1,6 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setClearColor( 0xffffff, 0);
 var light = new THREE.HemisphereLight(0xffffff,0,0.6);
 var ism = false;
 
