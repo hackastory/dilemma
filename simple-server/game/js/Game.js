57231936d360d5265900fa559625e7e4a6ea6cd5
frenkie <frank@frankbosma.nl>
2015-11-05 01:53:18 +0100

    cleanup and more explicit readme

--- simple-server/game/js/Game.js
+++ simple-server/game/js/Game.js
@@ -2,47 +2,24 @@
 
     var dl = document.location;
     var socketServer = dl.origin;
-
     var socket = io( socketServer );
-
-    var $gameContainer = $('#app');
 
     var Game = {
 
         setup: function () {
-        
-            socket.on('reset', Game.handleReset );
-            socket.on('playertaken', Game.handlePlayerTaken );
 
             ThreeDeeWorld.create();
 
-            Game.createChooser();
-        },
-
-        createChooser: function () {
-
-            // Use the 3D Chooser
             PlayerChooser.setup( socket );
 
             // Or choose one straight away
             //    DepressedGame.setup( socket );
             //    socket.emit('login', 'depressed');
-
-        },
-
-        createOutro: function () {
-
-        },
-
-        handlePlayerTaken: function ( playerName ) {
-            $('#'+ playerName ).hide(); // hide the button option
-        },
-
-        handleReset: function () {
+            //
+            //    ManicGame.setup( socket );
+            //    socket.emit('login', 'manic');
 
         }
-
-        
     };
 
 
