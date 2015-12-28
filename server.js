1e728772f9be6a57f8f84bc6c82f9ed96deeeca4
frenkie <frank@frankbosma.nl>
2015-12-28 12:19:47 +0100

    preparing for Heroku deployment

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ var app = express();
 var server = require('http').Server( app );
 var io = socketIo( server );
 
+    // binding to 0.0.0.0 allows connections from any other computer in the network
+    // to your ip address
+var ipAddress = process.env.IP || '0.0.0.0';
 var port = process.env.PORT || 4000;
 
 app.use( express.static( __dirname +'/game' ) );
 app.use( '/admin', express.static( __dirname +'/admin' ) );
 
 
-
-    // binding to 0.0.0.0 allows connections from any other computer in the network
-    // to your ip address
-server.listen( port, '0.0.0.0', function () {
+server.listen( port, ipAddress, function () {
 
     console.log('game server started on localhost:'+ port );
 
